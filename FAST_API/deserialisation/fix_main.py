0c5b87c8b9504d24f009279cbf34fb210667bbb6
jaysaini987 <jay@vtxsecurity.com>
2023-03-27 17:47:54 +1100

    add regex match to json validation

--- FAST_API/deserialisation/fix_main.py
+++ FAST_API/deserialisation/fix_main.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI, status, Request
+import re
 import json
-
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/data")
 def read_file(request: Request):
     json_data = request.query_params['json']
+    json_pattern = r'^(\s*|\{.*\}|\[.*\])$'
+    if not re.match(json_pattern, json_data):
+        return "Invalid Json Data"
     try:
         json.loads(json_data)
     except ValueError as e:
         return "Invalid Json Data"
     return "Valid Json Data"
-
